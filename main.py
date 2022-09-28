39e2dceef3cefc95c6189851666bd604ba92bfc6
Hussin Tsouli <76669327+Hussin22@users.noreply.github.com>
2022-09-28 20:20:13 +0100

    add some pretty comments

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from pytube import Playlist
 
+# input to get Url from user
 playlist_url = input('Please enter your Playlist: ')
+
 p = Playlist(playlist_url)
 
 print(f'Downloading: {p.title}')
 
+#looping in each video playlist
 for video in p.videos:
     print(video.title)
     st = video.streams.get_highest_resolution()
